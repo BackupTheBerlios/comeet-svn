0084c2da08b0a3054dca1ca8e40f9f2db74142cc
xtingray <xtingray@a7071c86-834c-0410-baae-ee03eb229994>
2008-05-11 00:27:04 +0000

    Adding license...

--- comeet/CoMeet-Admin/src/com/kazak/comeet/admin/transactions/QuerySenderException.java
+++ comeet/CoMeet-Admin/src/com/kazak/comeet/admin/transactions/QuerySenderException.java
@@ -1,26 +1,26 @@
-package com.kazak.comeet.admin.transactions;
-
 /**
- * STException.java Creado el 30-ago-2004
+ *  This file is part of CoMeet: Tiny IM.
+ *
+ *  CoMeet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CoMeet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CoMeet.  If not, see <http://www.gnu.org/licenses/>.
  * 
- * Este archivo es parte de E-Maku
- * <A href="http://comunidad.qhatu.net">(http://comunidad.qhatu.net)</A>
- *
- * E-Maku es Software Libre; usted puede redistribuirlo y/o realizar
- * modificaciones bajo los terminos de la Licencia Publica General
- * GNU GPL como esta publicada por la Fundacion del Software Libre (FSF);
- * tanto en la version 2 de la licencia, o cualquier version posterior.
- *
- * E-Maku es distribuido con la expectativa de ser util, pero
- * SIN NINGUNA GARANTIA; sin ninguna garantia aun por COMERCIALIZACION
- * o por un PROPOSITO PARTICULAR. Consulte la Licencia Publica General
- * GNU GPL para mas detalles.
- * <br>
- * Informacion de la clase
- * <br>
- * @author <A href='mailto:felipe@qhatu.net'>Luis Felipe Hernandez</A>
- * @author <A href='mailto:cristian@qhatu.net'>Cristian David Cepeda</A>
+ * @author Luis Felipe Hernandez
+ * @author Cristian David Cepeda
+ * 
+ * Contact: comeet@kazak.com.co
  */
+
+package com.kazak.comeet.admin.transactions;
 
 public class QuerySenderException extends Exception {
 
--- comeet/CoMeet-Lib/src/com/kazak/comeet/lib/misc/ConfigFileNotLoadException.java
+++ comeet/CoMeet-Lib/src/com/kazak/comeet/lib/misc/ConfigFileNotLoadException.java
@@ -1,27 +1,27 @@
+/**
+ *  This file is part of CoMeet: Tiny IM.
+ *
+ *  CoMeet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  CoMeet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CoMeet.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Luis Felipe Hernandez
+ * @author Cristian David Cepeda
+ * 
+ * Contact: comeet@kazak.com.co
+ */
+
 package com.kazak.comeet.lib.misc;
 
-
-/**
- * ConfigFileNotLoadException.java Creado el 13-jul-2004
- * 
- * Este archivo es parte de E-Maku
- * <A href="http://comunidad.qhatu.net">(http://comunidad.qhatu.net)</A>
- *
- * E-Maku es Software Libre; usted puede redistribuirlo y/o realizar
- * modificaciones bajo los terminos de la Licencia Publica General
- * GNU GPL como esta publicada por la Fundacion del Software Libre (FSF);
- * tanto en la version 2 de la licencia, o cualquier version posterior.
- *
- * E-Maku es distribuido con la expectativa de ser util, pero
- * SIN NINGUNA GARANTIA; sin ninguna garantia aun por COMERCIALIZACION
- * o por un PROPOSITO PARTICULAR. Consulte la Licencia Publica General
- * GNU GPL para mas detalles.
- * <br>
- * Informacion de la clase
- * <br>
- * @author <A href='mailto:felipe@qhatu.net'>Luis Felipe Hernandez</A>
- * @author <A href='mailto:cristian@qhatu.net'>Cristian David Cepeda</A>
- */
 public class ConfigFileNotLoadException extends Exception {
 
 	/**
